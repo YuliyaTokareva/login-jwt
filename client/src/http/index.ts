b3519b983478be3616c99e4f87df80bf8fd0669d
Yuliya Tokareva <ta4tonado@gmail.com>
2023-05-01 15:34:57 +0300

    test auth

--- client/src/http/index.ts
+++ client/src/http/index.ts
@@ -1,28 +1,28 @@
 import axios from 'axios';
 export const API_URL = process.env.REACT_APP_API_URL;
 
-// const $api = axios.create({
-//   withCredentials: true,
-//   baseURL: API_URL
-// });
-// $api.interceptors.request.use((config) => {
-//   config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
-//   return config;
-// });
-// export default $api;
-
-const api = axios.create({
-  baseURL: API_URL,
-  withCredentials: true
+const $api = axios.create({
+  withCredentials: true,
+  baseURL: API_URL
 });
-
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    config.headers['Authorization'] = `Bearer ${token}`;
-    config.headers['Cookie'] = `token=${token}`;
-  }
+$api.interceptors.request.use((config) => {
+  config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
   return config;
 });
+export default $api;
 
-export default api;
+// const api = axios.create({
+//   baseURL: API_URL,
+//   withCredentials: true
+// });
+
+// api.interceptors.request.use((config) => {
+//   const token = localStorage.getItem('token');
+//   if (token) {
+//     config.headers['Authorization'] = `Bearer ${token}`;
+//     config.headers['Cookie'] = `token=${token}`;
+//   }
+//   return config;
+// });
+
+// export default api;
