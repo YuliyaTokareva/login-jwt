8c2485c585ae68eb31ae67c980f2fa496eb04879
Yuliya Tokareva <ta4tonado@gmail.com>
2023-05-09 14:35:37 +0300

    add some settings in back

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -62,10 +62,7 @@ class UserController {
       const userData = await userService.refresh(refreshToken);
       res.cookie('refreshToken', userData.refreshToken, {
         maxAge: 30 * 24 * 60 * 60 * 1000,
-        httpOnly: true,
-        domain: '.netlify.app',
-        secure: true,
-        sameSite: 'none'
+        httpOnly: true
       });
       console.log(res);
       return res.json(userData);
